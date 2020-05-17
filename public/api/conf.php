98ae7937eef9b493c73d8886874e5a4d0fb7a940
David <david.moedinger@ketzu.net>
2020-05-17 16:25:23 +0200

    move api to public again and replace DB_ with environment variables for dockerization

--- public/api/conf.php
+++ public/api/conf.php
@@ -2,10 +2,19 @@
 header('Content-type: application/json');
 
 ########## Database Info ##########
-$db_server = 'DB_SERVER';
-$db_name = 'DB_NAME';
-$db_user = ' DB_USER';
-$db_passwd = 'DB_PASSWD';
+$db_server = $_ENV["DB_SERVER"]?: '';
+$db_name = $_ENV["DB_NAME"]?: '';
+$db_user = $_ENV["DB_USER"]?: '';
+$db_passwd = $_ENV["DB_PASSWORD"]?: '';
+
+if($db_server == '')
+	error_log("DB_SERVER not set.");
+if($db_name == '')
+	error_log("DB_NAME not set.");
+if($db_user == '')
+	error_log("DB_USER not set.");
+if($db_passwd == '')
+	error_log("DB_PASSWORD not set.");
 
 ########## READ DATA
 
