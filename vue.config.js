93cb2ced54a04e8de1d810d0382c36027efe6b4d
David <david.moedinger@ketzu.net>
2022-03-09 20:35:02 +0100

    Version updates and config for service workers to not precache /api/

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,11 @@ module.exports = {
   lintOnSave: false,
   pwa: {
     name: 'Rough Budget',
-    themeColor: '#151515'
+    themeColor: '#151515',
+    msTileColor: '#151515',
+    workboxPluginMode: 'GenerateSW',
+    workboxOptions: {
+      exclude: [new RegExp('api/*.*')],
+    }
   }
 }
